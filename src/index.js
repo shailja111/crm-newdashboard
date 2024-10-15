4c6dc3042e2445436d785849c56abaa605ab8d28
Shailja singh <singh.shailja9591@gmail.com>
2024-10-15 22:06:39 +0530

    Your commit message

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <DndProvider backend={HTML5Backend}>
-    <App />
-    </DndProvider>
- 
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
